97daa6802a8a25d4cd76770133eae1ec8f941a41
agerista <age.crawford@comcast.net>
2016-08-29 18:07:20 -0700

    Add files via upload

---
+++ ex3a.py
@@ -0,0 +1,33 @@
+# This is a string.
+print "I will now count my chickens:"
+
+#This follows PEMDAS to give you the number of animals
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+#This is another string.
+print "Now I will count the eggs:"
+
+#Once again PEMDAS, no floating numbers, just ints
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Ask a question in a string.
+print "Is that true that 3 + 2 < 5 - 7?"
+
+#Get an answer 
+print 3 + 2 < 5 - 7
+
+#Asking and answering
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+#negative numbers are not larger
+print "Oh, that's why it's False."
+
+#How about it?
+print "How about some more."
+
+#True or False?
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
